839d097c38d19596468a31a78077f145caf01446
Anshver1176 <192367970+Anshver1176@users.noreply.github.com>
2026-09-21 13:07:52 +0530

    Time: 1 ms (85.38%), Space: 46.7 MB (98.06%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -0,0 +1,38 @@
+class Solution {
+
+    private void bfs(int i, boolean[] visit, int[][] adj) {
+        int n = adj.length;
+
+        visit[i] = true;
+
+        Queue<Integer> q = new LinkedList<>();
+        q.add(i);
+
+        while (!q.isEmpty()) {
+            int front = q.remove();
+
+            for (int j = 0; j < n; j++) {
+                if (adj[front][j] == 1 && visit[j] == false) {
+                    q.add(j);
+                    visit[j] = true;
+                }
+            }
+        }
+    }
+
+    public int findCircleNum(int[][] adj) {
+        int n = adj.length;
+        int count = 0;
+
+        boolean[] visit = new boolean[n];
+
+        for (int i = 0; i < n; i++) {
+            if (!visit[i]) {
+                bfs(i, visit, adj);
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
